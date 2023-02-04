f7c5810d4945d20ac64d46c4b77dcd30052797f6
AhmedSHussaini <hussaahm@sheridancollege.ca>
2023-02-03 22:46:22 -0500

    Added Time based greeting

--- script.js
+++ script.js
@@ -88,3 +88,15 @@ const cursor = document.querySelector('.cursor');
                 cursor.classList.remove("expand");
             }, 500)
         })
+
+
+// Time based Greeting Script
+let container = document.querySelector(".greeting");
+let timeNow = new Date().getHours();
+let greeting = 
+    timeNow >= 5 && timeNow < 12
+    ? "Good Morning"
+    : timeNow >= 12 && timeNow < 18
+    ? "Good Afternoon"
+    : "Good Evening";
+    container.innerHTML = `<span>${greeting}</span>`
